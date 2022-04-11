0c45ea57ec6b8283a0488477a80b63425ffb18f3
jgomezRobotnik <jgomez@robotnik.es>
2022-04-11 15:55:58 +0200

    Add recoil library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  RecoilRoot,
+  useRecoilState,
+  useRecoilValue,
+  atom,
+  selector,
+} from 'recoil'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <TextInput />
+      <Counter />
+    </RecoilRoot>
   );
+}
+
+const textInputState = atom({
+  key: 'textInputState',
+  default: '',
+})
+
+const textInputSelector = selector({
+  key: 'textInputSelector',
+  get: ({ get }) => {
+    const text = get(textInputState)
+
+    return text.length
+  }
+})
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textInputState)
+
+  const onInputChange = (input) => {
+    setText(input.target.value)
+  }
+
+  return(
+    <div>
+      <input value={text} onChange={onInputChange}/>
+    </div>
+  )
+}
+
+function Counter() {
+  const length = useRecoilValue(textInputSelector)
+  return (
+    <span>{length}</span>
+  )
 }
 
 export default App;
